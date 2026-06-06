267320c921e61060298e1d7ed2e84ff8ab157aeb
hopperelec <hopperelec@gmail.com>
2026-06-06 15:10:08 +0100

    Monitor for entire new apps in addition to updated to the Pop app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {existsSync, readFileSync} from "node:fs";
-import googlePlayStore, {IFnAppOptions} from "google-play-scraper";
+import googlePlayStore from "google-play-scraper";
 import appleAppStore from "app-store-scraper";
 import {WebhookClient, APIEmbedField} from "discord.js";
 import 'dotenv/config';
@@ -14,24 +14,27 @@ const discordWebhookClient = DISCORD_WEBHOOK_URL ? new WebhookClient({ url: DISC
 
 const LAST_VERSIONS_FILE = 'last-versions.json';
 const CHECK_INTERVAL_MS = 1000 * 60 * 5; // Check every 5 minutes
-const DISCORD_FIELD_VALUE_LIMIT = 1024;
-const APPS: {
-    android: IFnAppOptions,
-    ios: { id: number; country: string } // app-store-scraper doesn't have type definitions
-} = {
-    android: { appId: 'uk.co.nebulalabs.nexusnextgeneration', lang: 'en', country: 'gb' },
-    ios: { id: 1169044288, country: 'gb' }
+const DISCORD_FIELD_VALUE_MAX_LENGTH = 1024;
+const DISCORD_EMBED_DESCRIPTION_MAX_LENGTH = 4096;
+const POP_APP_IDS = {
+    android: 'uk.co.nebulalabs.nexusnextgeneration',
+    ios: 1169044288,
 };
+const COUNTRY = 'gb';
 
-interface Version {
-    updated: number;
-    version: string;
+interface StoreData {
+    popVersion: {
+        updated: number;
+        version: string;
+        devId: string;
+    } | null;
+    otherApps: string[];
 }
 
 const lastVersions: {
     checkedAt: number | null;
-    android: Version | null;
-    ios: Version | null;
+    android: StoreData | null;
+    ios: StoreData | null;
 } = {
     checkedAt: null ,
     android: null,
@@ -48,34 +51,40 @@ if (existsSync(LAST_VERSIONS_FILE)) {
     }
 }
 
+function truncate(value: string, maxLength: number): string {
+    if (value.length > maxLength) {
+        const truncatedSuffix = `... (truncated)`
+        return value.substring(0, maxLength - truncatedSuffix.length) + truncatedSuffix;
+    }
+    return value;
+}
+
 async function handleUpdate(
-    name: string,
+    platform: string,
     oldVersion: string | null,
     newVersion: string,
+    newTimestamp: Date,
     releaseNotes: string,
     url: string
 ) {
     function log(message: string, ) {
-        console.log(` [${name}] ${message}`);
+        console.log(` [${platform}] ${message}`);
     }
     async function postUpdate(fields: APIEmbedField[]) {
         if (discordWebhookClient) {
             await discordWebhookClient.send({
                 embeds: [{
-                    title: `${name} App Update`,
+                    title: `${platform} App Update`,
                     fields,
                     url,
-                    timestamp: new Date().toISOString()
+                    timestamp: newTimestamp.toISOString()
                 }]
             });
         }
     }
 
     if (!releaseNotes) releaseNotes = 'No release notes provided.';
-    if (releaseNotes.length > DISCORD_FIELD_VALUE_LIMIT) {
-        const truncatedSuffix = `... (truncated)`
-        releaseNotes = releaseNotes.substring(0, DISCORD_FIELD_VALUE_LIMIT - truncatedSuffix.length) + truncatedSuffix;
-    }
+    releaseNotes = truncate(releaseNotes, DISCORD_FIELD_VALUE_MAX_LENGTH);
 
     if (!oldVersion) {
         log(`Initial version recorded: ${newVersion}`);
@@ -95,48 +104,140 @@ async function handleUpdate(
     }
 }
 
+async function handleNewApp(
+    platform: string,
+    title: string,
+    summary: string,
+    icon: string,
+    url: string,
+) {
+    summary = truncate(summary, DISCORD_EMBED_DESCRIPTION_MAX_LENGTH);
+    console.log(` [${platform}] New app: ${title}`);
+    if (discordWebhookClient) {
+        await discordWebhookClient.send({
+            embeds: [{
+                title: `New ${platform} App: ${title}`,
+                description: summary,
+                url,
+                thumbnail: { url: icon },
+            }]
+        });
+    }
+}
+
+function getErrorHandler(platform: string) {
+    return (err: any) => console.error(` [${platform}] Error:`, err.message);
+}
+
 async function checkUpdates() {
     const newCheckedAt = new Date();
     console.log(`[${newCheckedAt.toISOString().substring(0, 19).replace('T', ' ')}] Checking for updates...`);
 
-    await Promise.all([
-        googlePlayStore.app(APPS.android).then(
+    const promises = [
+        googlePlayStore.app({
+            appId: POP_APP_IDS.android,
+            country: COUNTRY
+        }).then(
             async androidData => {
-                const updated = new Date(androidData.updated).getTime();
-                if (!lastVersions.android || updated > lastVersions.android.updated) {
+                const updatedDate = new Date(androidData.updated);
+                const updatedTime = updatedDate.getTime();
+                if (!lastVersions.android?.popVersion || updatedTime > lastVersions.android.popVersion.updated) {
                     await handleUpdate(
                         'Android',
-                        lastVersions.android?.version || null,
+                        lastVersions.android?.popVersion?.version || null,
                         androidData.version,
+                        updatedDate,
                         decodeHtmlEntities(androidData.recentChanges.replaceAll(/<br\s*\/?>/gi, '\n')),
-                        `https://play.google.com/store/apps/details?id=${APPS.android.appId}`
+                        `https://play.google.com/store/apps/details?id=${POP_APP_IDS.android}`
                     );
-                    lastVersions.android = {
-                        updated,
-                        version: androidData.version
+                    if (!lastVersions.android) {
+                        lastVersions.android = {
+                            popVersion: null,
+                            otherApps: []
+                        };
+                    }
+                    lastVersions.android.popVersion = {
+                        updated: updatedTime,
+                        version: androidData.version,
+                        devId: androidData.developerId.replaceAll('+', ' ')
                     };
                 }
             }
-        ).catch(console.error),
-        appleAppStore.app(APPS.ios).then(
+        ).catch(getErrorHandler('Android')),
+        appleAppStore.app({
+            id: POP_APP_IDS.ios,
+            country: COUNTRY
+        }).then(
             async iosData => {
-                const updated = new Date(iosData.updated).getTime();
-                if (!lastVersions.ios || updated > lastVersions.ios.updated) {
+                const updatedDate = new Date(iosData.updated);
+                const updatedTime = updatedDate.getTime();
+                if (!lastVersions.ios?.popVersion || updatedTime > lastVersions.ios.popVersion.updated) {
                     await handleUpdate(
                         'iOS',
-                        lastVersions.ios?.version || null,
+                        lastVersions.ios?.popVersion?.version || null,
                         iosData.version,
+                        updatedDate,
                         iosData.releaseNotes,
-                        `https://apps.apple.com/app/id${APPS.ios.id}`
+                        `https://apps.apple.com/app/id${POP_APP_IDS.ios}`
                     );
-                    lastVersions.ios = {
-                        updated,
-                        version: iosData.version
+                    if (!lastVersions.ios) {
+                        lastVersions.ios = {
+                            popVersion: null,
+                            otherApps: []
+                        };
+                    }
+                    lastVersions.ios.popVersion = {
+                        updated: updatedTime,
+                        version: iosData.version,
+                        devId: iosData.developerId
                     };
                 }
             },
-        ).catch(console.error)
-    ]);
+        ).catch(getErrorHandler('iOS'))
+    ];
+    if (lastVersions.android?.popVersion) {
+        promises.push(
+            googlePlayStore.developer({
+                devId: lastVersions.android.popVersion.devId,
+                country: COUNTRY,
+            }).then(devApps => {
+                const otherApps = devApps.filter(app => app.appId !== POP_APP_IDS.android);
+                for (const app of otherApps) {
+                    if (lastVersions.android?.otherApps.includes(app.appId)) continue;
+                    handleNewApp(
+                        'Android',
+                        app.title,
+                        app.summary,
+                        app.icon,
+                        `https://play.google.com/store/apps/details?id=${app.appId}`
+                    ).catch(console.error);
+                    lastVersions.android?.otherApps.push(app.appId);
+                }
+            }).catch(getErrorHandler('Android'))
+        );
+    }
+    if (lastVersions.ios?.popVersion) {
+        promises.push(
+            appleAppStore.developer({
+                devId: lastVersions.ios.popVersion.devId,
+                country: COUNTRY,
+            }).then(devApps => {
+                const otherApps = devApps.filter(app => app.id !== POP_APP_IDS.ios);
+                for (const app of otherApps) {
+                    if (lastVersions.ios?.otherApps.includes(app.id.toString())) continue;
+                    handleNewApp(
+                        'iOS',
+                        app.title,
+                        app.description,
+                        app.icon,
+                        `https://apps.apple.com/app/id${app.id}`
+                    ).catch(console.error);
+                    lastVersions.ios?.otherApps.push(app.id.toString());
+                }
+            }).catch(getErrorHandler('iOS'))
+        );
+    }
+    await Promise.all(promises);
 
     console.log(' Check complete.');
     lastVersions.checkedAt = newCheckedAt.getTime();
